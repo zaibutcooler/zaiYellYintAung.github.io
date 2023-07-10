ad976821327390ab5c42d9130ef933ba070f3afa
Zai <zaiyellyintaung@gmail.com>
2023-07-11 05:07:32 +0630

    done pricing

--- main/src/components/ProjectDetail.vue
+++ main/src/components/ProjectDetail.vue
@@ -2,7 +2,7 @@
   <div
     class="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 z-50 px-2">
     <div
-      class="mt-8 bg-snow_white px-6 pb-6 pt-4 rounded-xl shadow-md lg:w-[600px] w-[350px] md:w-[500px] min-h-[400px] text-secondary_bold">
+      class="mt-8 bg-snow_white px-6 pb-6 pt-4 rounded-3xl shadow-md lg:w-[600px] w-[350px] md:w-[500px] min-h-[400px] text-secondary_bold">
       <div class="flex justify-end m-0 p-0">
         <button class="" @click="close">
           <i class="fas fa-xmark"></i>
