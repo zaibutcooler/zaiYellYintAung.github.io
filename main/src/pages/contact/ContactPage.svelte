3cb4075fb142927e211b4f4b08e1d69d8760d9cc
Zai <zaiyellyintaung@gmail.com>
2023-08-20 01:21:18 +0630

    done contact card

--- main/src/pages/contact/ContactPage.svelte
+++ main/src/pages/contact/ContactPage.svelte
@@ -3,7 +3,107 @@
 </script>
 
 <div  id="contact" class="flex justify-center w-full min-h-screen pt-14">
-    <main class="container max-w-full md:max-w-[1000px] px-3 md:px-6  ">
-        Contact Page
+    <main class="container max-w-full md:max-w-[1100px] px-3 md:px-6  ">
+
+        <div class="flex flex-col-reverse md:flex-row gap-8 w-full">
+
+         
+        <!-- <section class="w-full md:w-1/2 dark:bg-super_black dark:text-bg_white rounded-xl h-[420px] p-6">
+        
+            <h1 class="font-bold text-3xl mb-6">Get in touch</h1>
+            <p class="font-medium dark:text-bg_white leading-8 mb-8">
+              Have any questions or feedback? I'd love to hear from you. Reach
+              out to me using one of the methods below. I am a software engineer
+              who loves making everyone's lives easier. Thanks for exploring!
+            </p>
+            <div class="dark:text-bg_white font-medium">
+              <div class="flex items-center mb-5">
+                <i class="fas fa-globe text-xl font-semibold mr-8"></i>
+                <p class="">
+                  <a href="https://zaiyellyintaung.github.io/">
+                    zaiYellYintAung.github.io
+                  </a>
+                </p>
+              </div>
+              <div class="flex items-center mb-5">
+                <i class="fas fa-phone-alt text-xl font-semibold mr-8"></i>
+                <p>
+                  <a href="/">+95-9987304010</a>
+                </p>
+              </div>
+              <div class="flex items-center mb-5">
+                <i class="fas fa-envelope text-xl font-semibold mr-8"></i>
+                <p>
+                  <a href="/">zaiyellyintaung@gmail.com</a>
+                </p>
+              </div>
+            </div>
+
+
+            
+        </section>  -->
+        <section class="w-full md:w-1/2  min-h-[420px] p-5 md:p-8 border border-super_black rounded-2xl dark:bg-super_black">
+            <div class='mb-8 mt-2 md:mt-0'>
+                <h1 class="text-2xl md:text-3xl font-bold">Get In Touch With Me</h1>
+            </div>
+
+
+            <div class="mb-6 flex gap-4">
+                <div class="w-1/2">
+                  <label class="block text-gray-700 dark:text-bg_white text-sm font-bold mb-3" for="firstName">
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    class="px-3 py-2 border rounded dark:bg-bg_black dark:border-bg_white dark:focus:bg-super_black w-full"
+                    required
+                  />
+                </div>
+                <div class="w-1/2">
+                  <label class="block text-gray-700 dark:text-bg_white text-sm font-bold mb-3" for="lastName">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    class="px-3 py-2 border rounded dark:bg-bg_black dark:border-bg_white dark:focus:bg-super_black w-full"
+                    required
+                  />
+                </div>
+              </div>
+            
+              <div class="mb-6 w-full">
+                <label class="block text-gray-700 dark:text-bg_white text-sm font-bold mb-3" for="email">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="px-3 py-2 border rounded dark:bg-bg_black dark:border-bg_white dark:focus:bg-super_black w-full"
+                  required
+                />
+              </div>
+              <div class="mb-6 w-full flex-grow">
+                <label class="block text-gray-700 dark:text-bg_white text-sm font-bold mb-3" for="message">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  class="px-3 py-2 border rounded dark:bg-bg_black dark:border-bg_white dark:focus:bg-super_black w-full h-full"
+                  required></textarea>
+              </div>
+              <div class="w-full flex justify-end mt-8 ">
+                <button class="px-4 py-2 rounded-md bg-bg_white dark:bg-super_black text-snow_white border dark:hover:border-snow_white dark:hover:text-snow_white dark:border-bg_white ">
+                  Send Message
+                </button>
+              </div>
+        </section> 
+    </div>
+
     </main>
 </div>
