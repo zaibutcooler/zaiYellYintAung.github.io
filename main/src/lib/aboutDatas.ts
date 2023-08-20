06ffa5c2a00e33252f82c12dd1f2e4ce349d543e
Zai <zaiyellyintaung@gmail.com>
2023-08-20 23:05:03 +0630

    done work section

--- main/src/lib/aboutDatas.ts
+++ main/src/lib/aboutDatas.ts
@@ -1,7 +1,7 @@
 export const educationDatas = [
   {
     name: "Technological High-school (Lashio)",
-    degree: "High-school diploma with technology studies",
+    degree: "Technician Diploma",
     note: "",
     date: "2017 - 2019",
     logo: "",
@@ -16,10 +16,29 @@ export const educationDatas = [
   {
     name: "University of People (California)",
     degree: "Bachelor of Computer Science",
-    note: "Accredited distance-learning US-based University of People from Pasadena, California.",
+    note: "Accredited US-based distance-learning University from Pasadena, California.",
     date: "2022 - Present",
     logo: "",
   },
 ];
 
-export const workDatas = [];
+export const workDatas = [
+  {
+    name: "Than Htike Aung Construction Co., Ltd.",
+    position: "Junior Construction Engineer",
+    date: "2020-2021",
+    note: "",
+  },
+  {
+    name: "Konoike Construction Co., Ltd.",
+    position: "Sub-con Drafter",
+    date: "2021-2022",
+    note: "",
+  },
+  {
+    name: "Freelance As A Programmer",
+    position: "Freelancer",
+    date: "2022 - Present",
+    note: "",
+  },
+];
--- main/src/pages/about/AboutPage.svelte
+++ main/src/pages/about/AboutPage.svelte
@@ -15,23 +15,40 @@
         </section>
 
 
-        <section class="w-full md:w-2/5 ">
+        <section class="w-full md:w-2/5 text-snow_white ">
             <div class="md:w-full hidden md:block bg-white  border h-[350px] mb-6">
 
             </div>
-            <div class="md:w-full w-[350px] p-6 rounded-md dark:bg-super_black h-[320px] mb-6">
-                  <h1 class="font-semibold text-xl"><i class="fas fa-graduation-cap w-8"></i>Education</h1>
+            <div class="md:w-full w-[350px] p-6 rounded-2xl border  dark:border-border_black min-h-[320px] mb-6">
+                  <h1 class="font-semibold text-xl  mb-3"><i class="fas fa-graduation-cap w-8"></i>Education</h1>
                   {#each educationDatas as item (item.name)}
-                    <div>
-                        <h1>{item.name}</h1>
+                    <div class='p-2  mb-2 rounded-md'>
+                        <h1 class='mb-1'>{item.name}</h1>
+                        <p class="flex justify-between text-sm text-border_black mb-0.5">
+                            <span>{item.degree}</span>
+                            <span>{item.date}</span>
+                        </p>
+                        {#if item.note}
+                        <p class="text-[0.55rem] text-border_black">Note : {item.note}</p>
+                        {/if}
                     </div>
                   {/each}
             </div>
-            <div class="md:w-full w-[350px] p-6 rounded-md dark:bg-super_black h-[250px] mb-6">
-                  <h1 class="font-semibold text-xl"><i class="fas fa-briefcase w-8"></i>Work</h1>  
-                    
-            </div>
-        
+            <div class="md:w-full w-[350px] p-6 rounded-2xl border  dark:border-border_black  min-h-[320px] mb-6">
+                <h1 class="font-semibold text-xl  mb-3"><i class="fas fa-briefcase w-8"></i>Work</h1>
+                {#each workDatas as item (item.date)}
+                  <div class='p-2  mb-2 rounded-md '>
+                      <h1 class='mb-1'>{item.name}</h1>
+                      <p class="flex justify-between text-sm text-border_black">
+                          <span>{item.position}</span>
+                          <span>{item.date}</span>
+                      </p>
+                      {#if item.note}
+                      <p class="text-[0.55rem] text-border_black">Note : {item.note}</p>
+                      {/if}
+                  </div>
+                {/each}
+          </div>
 
 
         </section>
