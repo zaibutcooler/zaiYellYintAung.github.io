b794048afb952e2b6c3ae040383c7048838df30b
Zai <zaiyellyintaung@gmail.com>
2023-08-26 04:26:53 +0630

    service section

--- main/src/lib/servicesDatas.ts
+++ main/src/lib/servicesDatas.ts
@@ -1,3 +1,60 @@
-const serviceDatas = [];
+const serviceDatas = [
+  {
+    title: "Transform Your Ideas into Stunning Websites",
+    description:
+      "In the world of digital presence, a well-crafted website is essential to attract and engage customers. Our web development service brings your vision to life with modern designs and seamless functionality.",
+    duration: "4 weeks",
+    type: "Web Development",
+    note: "Responsive layouts for optimal user experience.",
+  },
+  {
+    title: "Crafting Intuitive Mobile Apps for Your Business",
+    description:
+      "Capture the mobile market with our top-notch mobile app development service. We design and build apps that cater to user needs while aligning with your business goals.",
+    duration: "6 weeks",
+    type: "Mobile Development",
+    note: "Cross-platform compatibility for wider reach.",
+  },
+  {
+    title: "Enhance User Interaction with Elegant UI/UX Designs",
+    description:
+      "User experience is paramount. Our UI/UX design service focuses on creating interfaces that not only look captivating but also provide seamless user journeys, resulting in happy and loyal users.",
+    duration: "3 weeks",
+    type: "Web Development",
+    note: "Iterative design process for optimal results.",
+  },
+  {
+    title: "Launching Your Online Store with E-Commerce Expertise",
+    description:
+      "Embark on your e-commerce journey with confidence. Our e-commerce solution service covers everything from product showcasing to secure payment gateways, empowering your business to thrive online.",
+    duration: "5 weeks",
+    type: "Web Development",
+    note: "Scalable architecture for future growth.",
+  },
+  {
+    title: "AI Development for Intelligent Solutions",
+    description:
+      "Leverage the power of AI to create smart applications that automate processes, analyze data, and make informed decisions. Our AI development service transforms your ideas into intelligent solutions.",
+    duration: "8 weeks",
+    type: "AI Development",
+    note: "Custom AI algorithms tailored to your needs.",
+  },
+  {
+    title: "Web Scraping Services for Data Retrieval",
+    description:
+      "Access valuable data from websites efficiently with our web scraping services. We gather and deliver the information you need, empowering your business with actionable insights.",
+    duration: "2 weeks",
+    type: "Web Development",
+    note: "Ethical and efficient data extraction practices.",
+  },
+  {
+    title: "Building Machine Learning Models for Predictive Insights",
+    description:
+      "Make sense of your data with machine learning models. Our service develops predictive models that uncover patterns, trends, and insights, enabling you to make informed decisions.",
+    duration: "6 weeks",
+    type: "AI Development",
+    note: "Data preprocessing, model training, and validation.",
+  },
+];
 
 export default serviceDatas;
--- main/src/pages/services/ServicePage.svelte
+++ main/src/pages/services/ServicePage.svelte
@@ -1,13 +1,81 @@
 <script>
-import ThemeToggleButton from "../_components/mini/button/ThemeToggleButton.svelte";
-</script>
+    import serviceDatas from './../../lib/servicesDatas';
+  
+    const aiDatas = serviceDatas.filter(item => item.type === 'AI Development');
+    const webDatas = serviceDatas.filter(item=>item.type === 'Web Development')
+    const mobileDatas = serviceDatas.filter(item=>item.type === 'Mobile Development')
+  </script>
+  
+  <div id="services" class="flex justify-center w-full min-h-screen pt-14">
+    <main class="container max-w-full md:max-w-[1000px]">
+      <section class="px-3 md:px-6 mb-6">
+        <h1 class="text-3xl md:text-4xl font-semibold md:font-bold text-primary mb-3 md:mb-6 leading-10 md:leading-12 flex gap-3">
+          Digital Solutions Tailored to You
+        </h1>
+        <p class="text-sm md:text-base md:pr-6 mb-6 leading-6 text-bg_white w-full md:w-3/4">
+          Experience tailored digital solutions designed just for you. I'm here to understand your goals and aspirations, whether it's a website, app, or strategy. Welcome to a partnership where your vision takes the lead.
+        </p>
+      </section>
+  
+      <section class="w-full md:w-5/6 pl-0 md:pl-5 ">
+        <div class="block md:flex">
+          <div class="w-full md:w-1/3 py-4 px-3 md:px-6 border-l dark:border-border_black">
+            <h1 class="pl-0 md:pl-4 font-semibold text-[0.9rem]">
+              Ai Development
+            </h1>
+          </div>
+          <div class="w-full md:w-2/3 px-0 md:px-6">
+            {#each aiDatas as data}
+              <main class="mb-4 p-3 md:p-5 md:rounded-md hover:bg-super_black transition-colors group duration-500 ease-in-out cursor-pointer">
+                <p class="dark:text-border_black mb-4">| In {data.duration}</p>
+                <h1 class="dark:text-snow_white font-semibold mb-3">{data.title}</h1>
+                <p class="text-sm dark:text-border_black leading-6 mb-3">{data.description}</p>
+                <button class="text-sm font-semibold dark:text-primary">See Details</button>
+              </main>
+            {/each}
+          </div>
+        </div>
 
-<div  id="services" class="flex justify-center w-full min-h-screen pt-14">
-    <main class="container max-w-full md:max-w-[1000px] px-3 md:px-6  ">
-        <section>
-            <h1 class='text-3xl md:text-4xl font-semibold md:font-bold text-primary mb-3 md:mb-6 leading-10 md:leading-12 flex gap-3'>Crafting Digital Solutions Tailored to You</h1>
-            <p class="text-sm md:text-base md:pr-6 mb-6 leading-6 text-bg_white w-full md:w-3/4">Experience tailored digital solutions designed just for you. I'm here to understand your goals and aspirations, whether it's a website, app, or strategy. Welcome to a partnership where your vision takes the lead.</p>
-        </section>
-    
+        <div class='py-4'/>
+
+        <div class="block md:flex">
+            <div class="w-full md:w-1/3 py-4 px-3 md:px-6 border-l dark:border-border_black">
+              <h1 class="pl-0 md:pl-4 font-semibold text-[0.9rem]">
+                Web Development
+              </h1>
+            </div>
+            <div class="w-full md:w-2/3 px-0 md:px-6">
+              {#each webDatas as data}
+                <main class="mb-4 p-3 md:p-5 md:rounded-md hover:bg-super_black transition-colors group duration-500 ease-in-out cursor-pointer">
+                  <p class="dark:text-border_black mb-4">| In {data.duration}</p>
+                  <h1 class="dark:text-snow_white font-semibold mb-3">{data.title}</h1>
+                  <p class="text-sm dark:text-border_black leading-6 mb-3">{data.description}</p>
+                  <button class="text-sm font-semibold dark:text-primary">See Details</button>
+                </main>
+              {/each}
+            </div>
+          </div>
+
+          <div class='py-4'/>
+
+          <div class="block md:flex">
+            <div class="w-full md:w-1/3 py-4 px-3 md:px-6 border-l dark:border-border_black">
+              <h1 class="pl-0 md:pl-4 font-semibold text-[0.9rem]">
+                Mobile Development
+              </h1>
+            </div>
+            <div class="w-full md:w-2/3 px-0 md:px-6">
+              {#each mobileDatas as data}
+                <main class="mb-4 p-3 md:p-5 md:rounded-md hover:bg-super_black transition-colors group duration-500 ease-in-out cursor-pointer">
+                  <p class="dark:text-border_black mb-4">| In {data.duration}</p>
+                  <h1 class="dark:text-snow_white font-semibold mb-3">{data.title}</h1>
+                  <p class="text-sm dark:text-border_black leading-6 mb-3">{data.description}</p>
+                  <button class="text-sm font-semibold dark:text-primary">See Details</button>
+                </main>
+              {/each}
+            </div>
+          </div>
+      </section>
     </main>
-</div>
+  </div>
+  
